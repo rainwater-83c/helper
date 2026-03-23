372679f827e4fd7f1f96bc49b713df8916a3ff48
Ubuntu <collinziolko@hotmail.com>
2026-03-23 12:22:15 +0000

    abilities

--- cogs/abilities.py
+++ cogs/abilities.py
@@ -9,9 +9,9 @@ class Abilities(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    abilities = app_commands.Group(
-        name="abilities", 
-        description="Abilities.",
+    red = app_commands.Group(
+        name="red", 
+        description="Red abilities.",
 
         allowed_contexts=app_commands.AppCommandContext(
             guild=True, 
@@ -24,15 +24,158 @@ class Abilities(Cog):
         )
     )
 
+    yellow = app_commands.Group(
+        name="yellow", 
+        description="Yellow abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    green = app_commands.Group(
+        name="green", 
+        description="Green abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    cyan = app_commands.Group(
+        name="cyan", 
+        description="Cyan abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    blue = app_commands.Group(
+        name="blue", 
+        description="Blue abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    magenta = app_commands.Group(
+        name="magenta", 
+        description="Magenta abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    white = app_commands.Group(
+        name="white", 
+        description="White abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    black = app_commands.Group(
+        name="black", 
+        description="Black abilities.",
+
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, 
+            dm_channel=True, 
+            private_channel=True
+        ),
+        allowed_installs=app_commands.AppInstallationType(
+            guild=True,
+            user=True
+        )
+    )
+
+    # red abilities
     @app_commands.allowed_installs(users=True, guilds=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    @abilities.command(name="red", description="Abilities to do with corruption.")
-    @app_commands.choices(redability=[
-        app_commands.Choice(name='Corrupt', value='corrupt'),
-    ])
-    async def red(self, interaction: discord.Interaction, user: discord.User)
+    @red.command(name="corrupt", description="Corrupts something.")
+    async def corrupt(self, interaction: discord.Interaction, thing: str):
+        await interaction.response.send_message(f"{interaction.user.mention} corrupts {thing} with squares.")
 
-    
+    # yellow abilities
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @yellow.command(name="heat", description="Heats something.")
+    async def heat(self, interaction: discord.Interaction, thing: str):
+        await interaction.response.send_message(f"{interaction.user.mention} heats up {thing} by adding energy.")
+
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @yellow.command(name="cool", description="Cools something.")
+    async def cool(self, interaction: discord.Interaction, thing: str):
+        await interaction.response.send_message(f"{interaction.user.mention} cools down {thing} by removing energy.")
+
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @yellow.command(name="eletrocute", description="Eletrocutes something.")
+    async def electrocute(self, interaction: discord.Interaction, thing: str):
+        await interaction.response.send_message(f"{interaction.user.mention} electrocutes {thing}.")
+
+    # green abilities
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @green.command(name="heal", description="Heals something.")
+    async def heal(self, interaction: discord.Interaction, thing: str):
+        await interaction.response.send_message(f"{interaction.user.mention} heals {thing}.")
+
+    # cyan abilities
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @cyan.command(name="teleport", description="Teleports to someplace or teleports something to someplace.")
+    async def teleport(self, interaction: discord.Interaction, thing: str, place: str):
+        await interaction.response.send_message(f"{interaction.user.mention} teleports {thing} to {place}.")
+
+    @app_commands.allowed_installs(users=True, guilds=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @cyan.command(name="scale", description="Changes the size of something or yourself.")
+    async def scale(self, interaction: discord.Interaction, thing: str, scale: int):
+        await interaction.response.send_message(f"{interaction.user.mention} scales {thing} to {scale} times it's original size.")
+
+
 
 async def setup(bot):
     await bot.add_cog(Main(bot))
